d145c03242302918ea9ac23ed0902834a0454da4
Takayuki0x <soummaneanass@gmail.com>
2024-12-25 02:10:58 +0100

    Changed API link

--- src/hooks/AuthProvider.jsx
+++ src/hooks/AuthProvider.jsx
@@ -10,7 +10,7 @@ const AuthProvider = ({ children }) => {
   const navigate = useNavigate();
   const loginAction = async (username, password) => {
     try {
-      const response = await fetch("http://localhost:8080/api/auth/login", {
+      const response = await fetch("https://medihack2024.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
